f701802e6326e1043db4dc2527ba1ec4cd35a8eb
Jones <0094294@students.dadeschools.net>
2018-04-02 12:05:49 -0400

    finished but error at the bottom (string command not blue)

--- logic.js
+++ logic.js
@@ -26,5 +26,78 @@ lettersInChosenWord = chosenWord.split("");
 numBlanks = lettersInChosenWord.length;
 console.log(chosenWord);
 
+/* sets score back to 0 */
+blanksAndSuccesses = [];
+wrongGuesses = [];
+for (var i = 0; i < numBlanks i++) {
+	blanksAndSuccesses.push("_");
+	}
+	console.log(blanksAndSuccesses)
+
+/* sets guesses to 9 */
+document.getElementById("guesses left").innerHTML = numGuesses
+/* prints guesses */                               /* what ever is in the ("") is how it will be seperated */
+document.getElementById("word blanks").innerHTML = blanksAndSuccesses.join(" ")
+/*prints wrong guesses*/
+document.getElementById("wrong guesses").innerHTML = wrongGuesses.join(" ")
 }
-startgame();
+
+/* new function */
+function checkletters(letter){
+	var letterInward = false
+	for (var i = 0; i < numBlanks; i++) {
+		if (chosenWord[i] === letter) {
+			letterInward = true 
+		}
+	}
+	if (letterInward) {
+		for (var j = 0 j < numBlanks j++) {
+			if (chosenWord[j] === letter){
+				blanksAndSuccesses[j] = letter;
+			}
+		}
+	console.log(blanksAndSuccesses)
+	}
+	else {
+		wrongGuesses.push(letter);
+		numGuesses --;
+	}
+}
+
+/* win and lose function */
+function roundComplete() {
+	console.log("WinCount: " + winCounter + " | LossCount " + lossCounter + " | NumGuesses" + numGuesses);
+
+	/* sets guesses to 9 */
+	document.getElementById("guesses left").innerHTML = numGuesses
+	/* prints guesses */                               /* what ever is in the ("") is how it will be seperated */
+	document.getElementById("word blanks").innerHTML = blanksAndSuccesses.join(" ")
+	/*prints wrong guesses*/
+	document.getElementById("wrong guesses").innerHTML = wrongGuesses.join(" ")
+
+	if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+		winCounter ++;
+		alert("You Win!");
+		document.getElementById("win-counter").innerHTML = winCounter;
+
+		startGame();
+	}
+	else if (numGuesses === 0) {
+		lossCounter ++;
+		alert("You Lose!");
+		document.getElementById("loss-counter").innterHTML = lossCounter;
+
+		startGame();
+	}
+}
+
+
+startGame();
+
+
+
+document.onkeyup = function(event) {
+	var letterGuessed = string.fromCharCode(event.keyCode).toLowerCase();
+	checkletters(letterGuessed);
+	roundComplete();
+};
